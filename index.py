754eb07dc38a5f4ffaef73abfe5a57b0d3a6d183
Young Moon <44722617+AndiCakolli@users.noreply.github.com>
2024-07-10 11:50:07 +0200

    Versioni i pare

---
+++ index.py
@@ -0,0 +1,6 @@
+# x = int(input("Shkruaje numrin e pikeve:")) 
+
+if ((x >= 0 ) and (x <= 29)):
+    print("Ti ke ngel edhe e ki noten 1")
+elif((x >= 30) and (x <= 49)):
+    print("Ti e ki noten 2sh")
